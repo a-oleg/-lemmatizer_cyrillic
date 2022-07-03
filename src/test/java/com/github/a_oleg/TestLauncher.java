6511b64e773b2845a6f69afd56b0ba5aef14142b
Oleg <oleg.arhipovski@gmail.com>
2022-07-03 13:25:45 +0300

    Write test

--- src/test/java/com/github/a_oleg/TestLauncher.java
+++ src/test/java/com/github/a_oleg/TestLauncher.java
@@ -1,16 +1,27 @@
 package com.github.a_oleg;
 
+import org.junit.platform.engine.discovery.DirectorySelector;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
 
 public class TestLauncher {
     public static void main(String[] args) {
         Launcher launcher = LauncherFactory.create();
-        LauncherDiscoveryRequest request = new LauncherDiscoveryRequestBuilder
-                .request()
-                .selectors();
-        launcher.execute(request);
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.
+                request().
+                selectors(DiscoverySelectors.selectPackage("com.github.a_oleg.service")).
+                build();
+        launcher.execute(request, summaryGeneratingListener);
+
+        try(PrintWriter writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
     }
 }
